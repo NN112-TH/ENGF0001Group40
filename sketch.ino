3a361133b8559c53b6785cae16bfefbb73fc599b
prithvikohligames <prithvikohligames@gmail.com>
2020-12-01 22:20:25 +0000

    improved stirring control

--- sketch.ino
+++ sketch.ino
@@ -18,85 +18,94 @@ const byte heaterPin     = 6;
 const byte thermistorPin = A0;
 const byte pHPin         = A1;
 
-float prev_stirring_time = 0;
-float prev_stirring_error = 0;
+float prevStirTime = 0;
+float prevStirErr = 0;
 
-float prev_rotation_time = 0;
-int rpm_count = 0;
-int rpm_sum = 0;
+float prevRotTime = 0;
+int rpmCount = 0;
+int rpmSum = 0;
 float rpm = 0;
-float prev_stirrer_pwm = 0;
+float prevStirPwm = 0;
 
 // The PCA9685 is connected to the default I2C connections. There is no need
 // to set these explicitly.
 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("Hello World");
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Hello World");
-
-  pinMode(lightgatePin,  INPUT);
-  pinMode(stirrerPin,    OUTPUT);
-  pinMode(heaterPin,     OUTPUT);
-  pinMode(thermistorPin, INPUT);
-  pinMode(pHPin,         INPUT);
+    pinMode(lightgatePin,  INPUT);
+    pinMode(stirrerPin,    OUTPUT);
+    pinMode(heaterPin,     OUTPUT);
+    pinMode(thermistorPin, INPUT);
+    pinMode(pHPin,         INPUT);
   
-  attachInterrupt(digitalPinToInterrupt(lightgatePin), lightgate_change, FALLING);
+    attachInterrupt(digitalPinToInterrupt(lightgatePin), lightgateChange, FALLING);
 }
 
-void lightgate_change() {
-  float current_time = millis();
-  float time_period = current_time - prev_rotation_time;
-  prev_rotation_time = current_time;
-  rpm_sum += 60 * (1 / (2 * (time_period / 1000)));
-  rpm_count++;
+void lightgateChange() 
+{
+    float now = millis();
+    float period = now - prevRotTime;
+    prevRotTime = now;
+    rpmSum += 60 * (1 / (2 * (period / 1000)));
+    rpmCount++;
 }
 
-void stirring_input() {
-  if (rpm_count >= 10) {
-    rpm = rpm_sum / rpm_count;
-    rpm_sum = 0;
-    rpm_count = 0;
-  }
+void stirInput() 
+{
+    if (rpmCount >= 15) 
+    {
+      rpm = rpmSum / rpmCount;
+      rpmSum = 0;
+      rpmCount = 0;
+    }
 }
 
-void stirring_output(float pwm) {
-  if (pwm > 0 && pwm < 255) {
-    analogWrite(stirrerPin, pwm);
-    prev_stirrer_pwm = pwm;
-  }
-  else if (pwm > 255) {
-    analogWrite(stirrerPin, 255);
-    prev_stirrer_pwm = 255;
-  }
-  else {
-    analogWrite(stirrerPin, prev_stirrer_pwm - 2);
-  }
+void stirOutput(float pwm) 
+{
+    if (pwm > 0 && pwm < 255) 
+    {
+      analogWrite(stirrerPin, pwm);
+      prevStirPwm = pwm;
+    }
+    else if (pwm > 255) 
+    {
+      analogWrite(stirrerPin, 255);
+      prevStirPwm = 255;
+    }
+    else 
+    {
+      analogWrite(stirrerPin, prevStirPwm);
+    }
 }
 
 
-float pid(float input, float setpoint, float* prev_time, float* prev_error, float k_p, float k_i, float k_d) {
-  float current_time = millis();
-  float elapsed_time = current_time - *prev_time;
-  *prev_time = current_time;
+float pid(float input, float setpoint, float* prevTime, float* prevErr, float kP, float kI, float kD) 
+{
+    float now = millis();
+    float elapsed = now - *prevTime;
+    *prevTime = now;
 
-  float error = setpoint - input;
-  float cum_error = error * elapsed_time;
-  float rate_error = (error - *prev_error) / elapsed_time;
-  *prev_error = error;
+    float error = setpoint - input;
+    float cumErr = error * elapsed;
+    float rateErr = (error - *prevErr) / elapsed;
+    *prevErr = error;
 
-  return k_p * error + k_i * cum_error + k_d * rate_error;
+    return kP * error + kI * cumErr + kD * rateErr;
 }
 
-void loop() {
-  float p_stirring = 0.05;
-  float i_stirring = 0.005;
-  float d_stirring = 0.7;
+void loop() 
+{
+    float pStir = 0.05;
+    float iStir = 0.005;
+    float dStir = 0.7;
   
-  float setpoint_stirring = 1000;
+    float setStir = 800;
   
-  stirring_input();
-  Serial.println(rpm);
-  float pid_stirring = pid(rpm, setpoint_stirring, &prev_stirring_time, &prev_stirring_error, p_stirring, i_stirring, d_stirring);
-  stirring_output(pid_stirring);
+    stirInput();
+    Serial.println(rpm);
+    float pidStir = pid(rpm, setStir-30, &prevStirTime, &prevStirErr, pStir, iStir, dStir);
+    stirOutput(pidStir);
 }
