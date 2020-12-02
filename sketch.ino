3cba394e6af881b920dd24b46966d28b396de859
prithvikohligames <prithvikohligames@gmail.com>
2020-12-02 01:55:14 +0000

    implemented heating subsystem control

--- sketch.ino
+++ sketch.ino
@@ -27,6 +27,15 @@ int rpmSum = 0;
 float rpm = 0;
 float prevStirPwm = 0;
 
+float prevHeatTime = 0;
+float prevHeatErr = 0;
+
+int tempCount = 0;
+int tempSum = 0;
+float temp = 0;
+
+unsigned long printTime = 5000;
+
 // The PCA9685 is connected to the default I2C connections. There is no need
 // to set these explicitly.
 
@@ -81,6 +90,30 @@ void stirOutput(float pwm)
     }
 }
 
+void heatInput()
+{
+    tempCount++;
+    tempSum += analogRead(thermistorPin);
+    
+    if (tempCount >= 30)
+    {
+      temp = (-0.0919 * (tempSum / tempCount)) + 345.14;
+      tempCount = 0;
+      tempSum = 0;
+    }
+}
+
+void heatOutput(float pwm)
+{
+    if (pwm > 0 && pwm < 255) 
+      analogWrite(heaterPin, pwm);
+      
+    else if (pwm > 255) 
+      analogWrite(heaterPin, 255);
+      
+    else 
+      analogWrite(heaterPin, 0);
+}
 
 float pid(float input, float setpoint, float* prevTime, float* prevErr, float kP, float kI, float kD) 
 {
@@ -98,7 +131,7 @@ float pid(float input, float setpoint, float* prevTime, float* prevErr, float kP
 
 void loop() 
 {
-    float pStir = 0.05;
+    /*float pStir = 0.05;
     float iStir = 0.005;
     float dStir = 0.7;
   
@@ -107,5 +140,16 @@ void loop()
     stirInput();
     Serial.println(rpm);
     float pidStir = pid(rpm, setStir-30, &prevStirTime, &prevStirErr, pStir, iStir, dStir);
-    stirOutput(pidStir);
+    stirOutput(pidStir);*/
+    
+    float pHeat = 20;
+    float iHeat = 0.0;
+    float dHeat = 0.0;
+  
+    float setHeat = 298;
+  
+    heatInput();
+    Serial.println(temp);
+    float pidHeat = pid(temp, setHeat, &prevHeatTime, &prevHeatErr, pHeat, iHeat, dHeat);
+    heatOutput(pidHeat);
 }
