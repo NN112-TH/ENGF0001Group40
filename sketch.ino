fd8af3b689e1e27e39ed0c5c1919e2f610942b78
prithvikohligames <prithvikohligames@gmail.com>
2020-12-09 02:26:09 +0000

    added pH subsystem

--- sketch.ino
+++ sketch.ino
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <Wire.h>
 
 // These define which pins are connected to what device on the virtual bioreactor
 //
@@ -17,6 +18,7 @@ const byte stirrerPin    = 5;
 const byte heaterPin     = 6;
 const byte thermistorPin = A0;
 const byte pHPin         = A1;
+const byte I2CADDR = 0x40;
 
 float prevStirTime = 0;
 float prevStirErr = 0;
@@ -34,7 +36,12 @@ int tempCount = 0;
 int tempSum = 0;
 float temp = 0;
 
-unsigned long printTime = 5000;
+float prevPHTime = 0;
+float prevPHErr = 0;
+
+int pHCount = 0;
+int pHSum  = 0;
+float pH = 0;
 
 // The PCA9685 is connected to the default I2C connections. There is no need
 // to set these explicitly.
@@ -51,6 +58,11 @@ void setup()
     pinMode(pHPin,         INPUT);
   
     attachInterrupt(digitalPinToInterrupt(lightgatePin), lightgateChange, FALLING);
+    
+    Wire.beginTransmission(I2CADDR);
+    Wire.write(0x00);
+    Wire.write(0x21);
+    Wire.endTransmission();
 }
 
 void lightgateChange() 
@@ -66,9 +78,9 @@ void stirInput()
 {
     if (rpmCount >= 15) 
     {
-      rpm = rpmSum / rpmCount;
-      rpmSum = 0;
-      rpmCount = 0;
+        rpm = rpmSum / rpmCount;
+        rpmSum = 0;
+        rpmCount = 0;
     }
 }
 
@@ -76,17 +88,17 @@ void stirOutput(float pwm)
 {
     if (pwm > 0 && pwm < 255) 
     {
-      analogWrite(stirrerPin, pwm);
-      prevStirPwm = pwm;
+        analogWrite(stirrerPin, pwm);
+        prevStirPwm = pwm;
     }
     else if (pwm > 255) 
     {
-      analogWrite(stirrerPin, 255);
-      prevStirPwm = 255;
+        analogWrite(stirrerPin, 255);
+        prevStirPwm = 255;
     }
     else 
     {
-      analogWrite(stirrerPin, prevStirPwm);
+        analogWrite(stirrerPin, prevStirPwm);
     }
 }
 
@@ -97,22 +109,79 @@ void heatInput()
     
     if (tempCount >= 30)
     {
-      temp = (-0.0919 * (tempSum / tempCount)) + 345.14;
-      tempCount = 0;
-      tempSum = 0;
+        temp = (-0.0919 * (tempSum / tempCount)) + 345.14;
+        tempCount = 0;
+        tempSum = 0;
     }
 }
 
 void heatOutput(float pwm)
 {
     if (pwm > 0 && pwm < 255) 
-      analogWrite(heaterPin, pwm);
+        analogWrite(heaterPin, pwm);
       
     else if (pwm > 255) 
-      analogWrite(heaterPin, 255);
+        analogWrite(heaterPin, 255);
       
     else 
-      analogWrite(heaterPin, 0);
+        analogWrite(heaterPin, 0);
+}
+
+void pHInput()
+{
+    pHCount++;
+    pHSum += analogRead(pHPin);
+    
+    if (pHCount >= 30)
+    {
+        pH = pHSum / pHCount;
+        pHCount = 0;
+        pHSum = 0;
+    }
+}
+
+void pHOutput(int on)
+{
+    if (on >= 0)
+    {
+        if (on > 4095)
+        {
+            on = 4095;
+        }
+        byte onLow = lowByte(on);
+        
+        int off = 4095 - on;
+        byte offLow = lowByte(off);
+        
+        Wire.beginTransmission(I2CADDR);
+        Wire.write(0x0A);
+        Wire.write(onLow);
+        Wire.write(on - onLow);
+        Wire.write(offLow);
+        Wire.write(off - offLow);
+        Wire.endTransmission();
+    }
+    else
+    {
+        on = abs(on);
+        if (on > 4095)
+        {
+            on = 4095;
+        }
+        byte onLow = lowByte(on);
+        
+        int off = 4095 - on;
+        byte offLow = lowByte(off);
+        
+        Wire.beginTransmission(I2CADDR);
+        Wire.write(0x06);
+        Wire.write(onLow);
+        Wire.write(on - onLow);
+        Wire.write(offLow);
+        Wire.write(off - offLow);
+        Wire.endTransmission();
+    }
+    
 }
 
 float pid(float input, float setpoint, float* prevTime, float* prevErr, float kP, float kI, float kD) 
@@ -142,14 +211,19 @@ void loop()
     float pidStir = pid(rpm, setStir-30, &prevStirTime, &prevStirErr, pStir, iStir, dStir);
     stirOutput(pidStir);*/
     
-    float pHeat = 20;
+    /*float pHeat = 20;
     float iHeat = 0.0;
     float dHeat = 0.0;
   
-    float setHeat = 298;
+    float setHeat = 310;
   
     heatInput();
     Serial.println(temp);
     float pidHeat = pid(temp, setHeat, &prevHeatTime, &prevHeatErr, pHeat, iHeat, dHeat);
-    heatOutput(pidHeat);
+    heatOutput(pidHeat);*/
+    
+    pHInput();
+    Serial.println(pH);
+    
+    pHOutput(-10);
 }
